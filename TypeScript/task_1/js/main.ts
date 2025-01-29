ef7bc0e4fe570c062fed5768ecaf5609e75b8b1f
only1techgal <jenn.lipholo@gmail.com>
2025-01-29 13:36:20 +0200

    2. Extending the Teacher class

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -20,3 +20,19 @@ const teacher1: teacher = {
 
 // Logs the teacher object
 console.log(teacher1);
+
+// Define the 'Director' interace that extends 'Teacher'
+interface Director extends teacher {
+    numberOfReports: number; // The new required property
+}
+// Create director object
+const director1: Director = {
+    firstName: "Paul",
+    lastName: "Rooberts",
+    location: "Nashville",
+    fullTimeEmployee: true,
+    numberOfReports: 60, // The required peroperty for Director
+}
+
+// Logs the director object
+console.log(director1);
