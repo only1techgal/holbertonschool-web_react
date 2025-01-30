3cec5d03ad1cd257dace74fc65a84b9301da7679
only1techgal <jenn.lipholo@gmail.com>
2025-01-30 14:54:13 +0200

    8. Ambient Namespaces

---
+++ TypeScript/task_3/interface.ts
@@ -0,0 +1,9 @@
+// Define type 'RowID' which equals to a number
+export type RowID = number;
+
+// Define an interface for 'RowElement'
+export interface RowElement {
+    firstName: string;
+    lastName: string;
+    age?: number;
+}
